e3cf5397841ee5b406f492f284f5021c7a116fe2
Anjaly1997 <anjaly1997soman@gmail.com>
2023-07-26 17:09:56 +0200

    improved pagination component

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -4,20 +4,20 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import {Stack, Pagination} from '@mui/material'
+import { Stack, Pagination } from '@mui/material';
 
 interface Startup {
-  id: number; 
+  id: number;
   name: string;
   shortDescription: string;
 }
 
-
 const StartupList = () => {
   const [startups, setStartups] = useState<Startup[]>([]);
+  const [page, setPage] = useState(1);
+  
+  const startupsPerPage = 2; 
 
- const [pageNumber, setPageNumber] = useState(2)
-  const [page, setPage] = React.useState(1);
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
@@ -25,10 +25,8 @@ const StartupList = () => {
   useEffect(() => {
     const fetchStartups = async () => {
       try {
-        console.log('started')
         const response = await axios.get('/api/startups');
         setStartups(response.data);
-        console.log(response.data);
       } catch (error) {
         console.error('Error fetching startups:', error);
         setStartups([]);
@@ -38,36 +36,44 @@ const StartupList = () => {
     fetchStartups();
   }, []);
 
-
   if (startups.length === 0) {
     return <div>No startups found.</div>;
   }
 
+  const totalPages = Math.ceil(startups.length / startupsPerPage);
+
+  const startIndex = (page - 1) * startupsPerPage;
+  const endIndex = startIndex + startupsPerPage;
+  const startupsToDisplay = startups.slice(startIndex, endIndex);
+
   return (
     <div>
       <Stack spacing={2}>
-      <Typography>Page: {page}</Typography>
-      <Pagination count={10} page={page} onChange={handleChange} />
-    </Stack>
+        <Typography>Page: {page}</Typography>
+        <Pagination count={totalPages} page={page} onChange={handleChange} />
+      </Stack>
       <Typography variant="h4" gutterBottom>
         Startup List
       </Typography>
 
-      {startups.map((startup: Startup) => ( 
-        if({startup.id} <2){}
-        <Box key={startup.id} sx={{ minWidth: 275 }}>
-          <Card variant="outlined">
-            <CardContent>
-              <Typography variant="h5" component="div">
-                {startup.name}
-              </Typography>
-              <Typography variant="body2">
-                {startup.shortDescription}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Box>
-      ))}
+      {startupsToDisplay.length === 0 ? (
+        <div>No startups found for this page.</div>
+      ) : (
+        startupsToDisplay.map((startup: Startup) => (
+          <Box key={startup.id} sx={{ minWidth: 275 }}>
+            <Card variant="outlined">
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  {startup.name}
+                </Typography>
+                <Typography variant="body2">
+                  {startup.shortDescription}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        ))
+      )}
     </div>
   );
 };
